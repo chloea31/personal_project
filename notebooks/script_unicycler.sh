354d82702d6126683e06461185588c56cb0c9b8c
Chloe Aujoulat <chloe.aujoulat19@gmail.com>
2023-11-05 16:52:06 +0100

    add new version of the script with functional code

--- notebooks/script_unicycler.sh
+++ notebooks/script_unicycler.sh
@@ -25,6 +25,7 @@ conda activate EnvVelvet
 
 SITE="https://zenodo.org/record/940733/files/"
 WORK_DIR=/home/Documents/Bioinformatique/personal_project/ # work directory
+FILES="illumina_f.fq illumina_r.fq minion_2d.fq"
 
 ####################
 ### Data collection
@@ -43,27 +44,37 @@ fi
 if [ ! -d $WORK_DIR/data/processed ]; then
     mkdir $WORK_DIR/data/processed
 fi
-for file in $SITE; do
-        
+
+for file in $FILES; do # for loop iterates over a table
+    if [ ! -f $WORK_DIR/data/raw/$file ]; then 
+        wget -P $WORK_DIR/data/raw/ $SITE/$file
+    fi      
 done 
 
-####################
-### Evaluate the input reads
-####################
-
-mkdir data
-mkdir raw
-cd data/raw/
-
-wget https://zenodo.org/record/940733/files/illumina_f.fq
-wget https://zenodo.org/record/940733/files/illumina_r.fq
-wget https://zenodo.org/record/940733/files/minion_2d.fq
+#wget https://zenodo.org/record/940733/files/illumina_f.fq
+#wget https://zenodo.org/record/940733/files/illumina_r.fq
+#wget https://zenodo.org/record/940733/files/minion_2d.fq
 
 du -sh *
 
-# Quality control of the data
-fastqc illumina_f.fq -o ../../reports/QC/
-fastqc illumina_r.fq -o ../../reports/QC/
+####################
+### Quality control of the data
+####################
+echo "> quality control of the data"
+
+#fastqc illumina_f.fq -o ../../reports/QC/
+#fastqc illumina_r.fq -o ../../reports/QC/
+#fastqc minion_2d.fq -o ../../reports/QC/
+
+for file in $WORK_DIR/data/raw/*.fq; do 
+    if [ ! -d $WORK_DIR/reports ]; then 
+        mkdir $WORK_DIR/reports
+    fi
+    if [ ! -d $WORK_DIR/reports/QC ]; then
+        mkdir $WORK_DIR/reports/QC
+    fi
+    fastqc $file -o $WORK_DIR/reports/QC/
+done
 
 # MultiQC on FASTQ files
 multiqc ../fastqc/*_fastqc.zip
@@ -78,6 +89,8 @@ grep ">" assembly.fasta | wc -l
 #################
 ### Assess Assembly quality with Quast
 #################
+
+
 
 #################
 ### Genome annotation using Prokka
