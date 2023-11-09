128688df0c9806fa9b41eae7bce336e1c83c0b12
Chloe Aujoulat <chloe.aujoulat19@gmail.com>
2023-11-09 15:49:50 +0100

    add new version of the script for velvet assembler with first part of the code functional

--- notebooks/script_velvet.sh
+++ notebooks/script_velvet.sh
@@ -6,35 +6,59 @@
 #######################################################################################################################################################
 #######################################################################################################################################################
 
+####################
+### Activate the working environment
+####################
 
-##########
-# Evaluate the input reads
-##########
+#conda activate EnvVelvet
 
-cd /mnt/d/Bioinformatique/personal_project
-mkdir data
-mkdir raw
-mkdir velvet
-cd data/raw/
+####################
+### Running the pipeline
+####################
 
-wget https://zenodo.org/record/582600/files/mutant_R1.fastq
-wget https://zenodo.org/record/582600/files/mutant_R2.fastq
-wget https://zenodo.org/record/582600/files/wildtype.fna
+# in /home/Documents/Bioinformatique/personal_project/ tape: ./notebooks/script_velvet.sh
 
-du -sh mutant_R1.fastq
-du -sh mutant_R2.fastq
-du -sh wildtype.fna
+####################
+### Variable initialization
+####################
 
-less mutant_R1.fastq
-less mutant_R2.fastq
-less wildtype.fna
+SITE="https://zenodo.org/record/582600/files"
+WORK_DIR=. # working directory
+FILES="mutant_R1.fastq mutant_R2.fastq wildtype.fna"
+#QUAST_DIR=/home/caujoulat/miniforge3/envs/EnvVelvet/bin
+
+####################
+### Data collection
+####################
+echo "> download the datasets"
+
+if [ ! -d $WORK_DIR/data ]; then
+    mkdir $WORK_DIR/data
+fi 
+if [ ! -d $WORK_DIR/data/raw ]; then 
+    mkdir $WORK_DIR/data/raw
+fi
+if [ ! -d $WORK_DIR/data/raw/velvet ]; then
+    mkdir $WORK_DIR/data/raw/velvet
+fi
+
+for file in $FILES; do # for loop iterates over a table
+    if [ ! -f $WORK_DIR/data/raw/velvet/$file ]; then 
+        wget -P $WORK_DIR/data/raw/velvet/ $SITE/$file
+    fi      
+done 
+
+#wget https://zenodo.org/record/582600/files/mutant_R1.fastq
+#wget https://zenodo.org/record/582600/files/mutant_R2.fastq
+#wget https://zenodo.org/record/582600/files/wildtype.fna
+
 
 # Quality control of the data
-fastqc mutant_R1.fastq
-fastqc mutant_R2.fastq
+#fastqc mutant_R1.fastq
+#fastqc mutant_R2.fastq
 
 # MultiQC on FASTQ files
-multiqc ../fastqc/*_fastqc.zip
+#multiqc ../fastqc/*_fastqc.zip
 
 ##########
 # Assemble reads with Velvet
@@ -44,31 +68,31 @@ multiqc ../fastqc/*_fastqc.zip
 # Use this link: https://usegalaxy.eu/root?tool_id=fastq_paired_end_interlacer
 
 # 2) velveth
-velveth ../reports/velvet/. 29 -shortPaired -fastq -interleaved ../data/interm/Galaxy5-\[FASTQ_interlacer_pairs_from_data_2_and_data_1\].fastqsanger
-less Log
-less Roadmaps
-less Sequences
+#velveth ../reports/velvet/. 29 -shortPaired -fastq -interleaved ../data/interm/Galaxy5-\[FASTQ_interlacer_pairs_from_data_2_and_data_1\].fastqsanger
+#less Log
+#less Roadmaps
+#less Sequences
 
 # 3) velvetg
-velvetg .
-less contigs.fa
-wc -l stats.txt
-tail -5 stats.txt
-grep -o '>' contigs.fa | wc -l
+#velvetg .
+#less contigs.fa
+#wc -l stats.txt
+#tail -5 stats.txt
+#grep -o '>' contigs.fa | wc -l
 
 ##########
 # Collect some statistics on the contigs
 ##########
 
 # Datamash
-cat stats.txt | datamash -H mean 2
-cat stats.txt | datamash -H min 2
-cat stats.txt | datamash -H max 2
+#cat stats.txt | datamash -H mean 2
+#cat stats.txt | datamash -H min 2
+#cat stats.txt | datamash -H max 2
 
 # Quast
-/home/caujoulat/miniconda3/envs/EnvVelvet/bin/quast.py ../reports/velvet/contigs.fa \
-    -o ../reports/quast/. \
-    -r ../data/raw/wildtype.fna \
-    --contig-thresholds 0.1000
+#/home/caujoulat/miniconda3/envs/EnvVelvet/bin/quast.py ../reports/velvet/contigs.fa \
+ #   -o ../reports/quast/. \
+ #   -r ../data/raw/wildtype.fna \
+ #   --contig-thresholds 0.1000
 
 
