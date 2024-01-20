baa562db7ae92fd5bdc8d6dfeef3d07ae384915b
Chloe Aujoulat <chloe.aujoulat19@gmail.com>
2024-01-20 20:13:08 +0100

    add new python script to parse fastqc files and get the dataframe on interest before plotting the graphs; rest to store the 1st dataframe as a csv file and plot the first graphs for illumina_f

---
+++ notebooks/unicycler/parsing_txt_files.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import pandas as pd
+
+with open("../../reports/unicycler/QC/illumina_f_fastqc/illumina_f_fastqc/fastqc_data.txt", "r") as f:
+    data = None
+    for row in f:
+        #if row.startswith(">>Sequence Length Distribution"):
+            #print(row)
+        new_row = row.rstrip("\n").replace("\t", ",")
+        #print(row_splited)
+        if "Sequence Length Distribution" in new_row:
+            data = []
+        elif data is not None:
+            if ">>END_MODULE" in new_row:
+                break
+            else:
+                data.append(new_row.split(","))
+    #print(data)
+
+df = pd.DataFrame(data).rename(columns = {0:"#Length", 1:"Count"})
+df = df[1:]
+print(df)
