2f8a07495aee788aefb368eab63c3517ba9e0991
Chloe Aujoulat <chloe.aujoulat19@gmail.com>
2024-01-21 17:15:34 +0100

    add new version script parsing file with less copy of the df

--- notebooks/unicycler/parsing_txt_files.py
+++ notebooks/unicycler/parsing_txt_files.py
@@ -18,8 +18,7 @@ with open("../../reports/unicycler/QC/illumina_f_fastqc/illumina_f_fastqc/fastqc
                 data.append(new_row.split(","))
     #print(data)
 
-df = pd.DataFrame(data).rename(columns = {0:"#Length", 1:"Count"}) # or (data[1:], columns = data[0])
-df = df[1:]
+df = pd.DataFrame(data[1:], columns = data[0]).rename(columns = {0:"#Length", 1:"Count"}) # or (data[1:], columns = data[0])
 print(df)
 
 df.to_csv("../../reports/unicycler/QC/illumina_f_fastqc/illumina_f_fastqc/illumina_f_seq_length_dist.csv", sep = ",", index = False)
