ceaee3980ff52854a6c0dc91f4a36a72a12549f4
Chloe Aujoulat <chloe.aujoulat19@gmail.com>
2024-02-28 22:40:42 +0100

    add first part of spades script with downloading the data; we might want to get the same Illumina data as the data used for velvet tutorial

--- notebooks/spades/spades_script.sh
+++ notebooks/spades/spades_script.sh
@@ -17,3 +17,33 @@
 ####################
 
 # in /home/Documents/Bioinformatique/personal_project/ tape: ./notebooks/spades/script_spades.sh
+
+####################
+### Variable initialization
+####################
+
+SITE="https://zenodo.org/record/582600/files" # let's use the same Illumina data as Velvet assembler
+WORK_DIR="$PWD" # working directory -> absolute way of the current (working) directory
+FILES="mutant_R1.fastq mutant_R2.fastq wildtype.fna"
+
+####################
+### Data collection
+####################
+echo "> download the datasets"
+
+if [ ! -d $WORK_DIR/data ]; then
+    mkdir "$WORK_DIR/data"
+fi 
+if [ ! -d $WORK_DIR/data/raw ]; then 
+    mkdir "$WORK_DIR/data/raw"
+fi
+if [ ! -d $WORK_DIR/data/raw/velvet ]; then
+    mkdir "$WORK_DIR/data/raw/velvet"
+fi
+
+for file in $FILES; do # for loop iterates over a table
+    if [ ! -f "$WORK_DIR/data/raw/velvet/$file" ]; then 
+        wget -P "$WORK_DIR/data/raw/velvet/" "$SITE/$file"
+    fi      
+done 
+
